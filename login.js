0e4d0ea040a2c6376270737e44fd4a026e82ae52
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-12 01:09:48 +0300

    <

--- login.js
+++ login.js
@@ -1,10 +1,46 @@
-const togglePassword = document.querySelector('#togglePassword');
-  const password = document.querySelector('#id_password');
+// const togglePassword = document.querySelector("#togglePassword");
+// const password = document.querySelector("#password");
 
-  togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
-});
+// togglePassword.addEventListener("ondblclick", function () {
+//   // toggle the type attribute
+//   const type = password.getAttribute("type") === "password" ? "text" : "password";
+//   password.setAttribute("type", type);
+
+//   // toggle the icon
+//   this.classList.toggle("bi-eye");
+// });
+// const togglePassword = document.querySelector('#togglePassword');
+// const password = document.querySelector('#password');
+
+// togglePassword.addEventListener('click', function (e) {
+//   // toggle the type attribute
+//   const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+//   password.setAttribute('type', type);
+//   // toggle the eye / eye slash icon
+//   this.classList.toggle('bi-eye');
+// });
+
+
+
+
+
+
+
+
+  // const togglePassword = document.querySelector("#togglePassword");
+  // const password = document.querySelector("#password");
+
+  // togglePassword.addEventListener("click", function () {
+  //           // toggle the type attribute
+  //           const type = password.getAttribute("type") === "password" ? "text" : "password";
+  // password.setAttribute("type", type);
+
+  // // toggle the icon
+  // this.classList.toggle("bi-eye");
+  //       });
+
+  // // prevent form submit
+  // const form = document.querySelector("form");
+  // form.addEventListener('submit', function (e) {
+  //   e.preventDefault();
+  //       });
