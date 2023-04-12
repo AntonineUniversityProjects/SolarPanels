ac1af85a30c9ea5d34452988325a326a982e4f35
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-12 21:55:43 +0300

    inserted the eye script into login.html

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
-// const togglePassword = document.querySelector("#togglePassword");
-// const password = document.querySelector("#password");
 
-// togglePassword.addEventListener("ondblclick", function () {
+
+// const togglePassword = document.querySelector("#togglePassword");
+// const password = document.querySelector(".pass #password");
+
+// togglePassword.addEventListener("click", function () {
 //   // toggle the type attribute
 //   const type = password.getAttribute("type") === "password" ? "text" : "password";
 //   password.setAttribute("type", type);
@@ -9,38 +11,9 @@
 //   // toggle the icon
 //   this.classList.toggle("bi-eye");
 // });
-// const togglePassword = document.querySelector('#togglePassword');
-// const password = document.querySelector('#password');
 
-// togglePassword.addEventListener('click', function (e) {
-//   // toggle the type attribute
-//   const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-//   password.setAttribute('type', type);
-//   // toggle the eye / eye slash icon
-//   this.classList.toggle('bi-eye');
+// // prevent form submit
+// const form = document.querySelector("form");
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
 // });
-
-
-
-
-
-
-
-
-  // const togglePassword = document.querySelector("#togglePassword");
-  // const password = document.querySelector("#password");
-
-  // togglePassword.addEventListener("click", function () {
-  //           // toggle the type attribute
-  //           const type = password.getAttribute("type") === "password" ? "text" : "password";
-  // password.setAttribute("type", type);
-
-  // // toggle the icon
-  // this.classList.toggle("bi-eye");
-  //       });
-
-  // // prevent form submit
-  // const form = document.querySelector("form");
-  // form.addEventListener('submit', function (e) {
-  //   e.preventDefault();
-  //       });
