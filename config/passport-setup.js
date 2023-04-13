33055532b33add14e8038dd1095af05e21e33191
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-13 04:49:50 +0300

    finished for today

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,13 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('./config/keys');
+const keys = require('./keys');
 passport.use(
     new GoogleStrategy({
         //option for the strategy
         callbackURL:"/auth/google/redirect",
-        clientID:"keys.google.clientID",
-        clientSecret:"keys.google.clientSecret"
+        clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com",
+        clientSecret:"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"
 
-    }), () => {
+    }, () => {
         // passport callback function
     })
+)
