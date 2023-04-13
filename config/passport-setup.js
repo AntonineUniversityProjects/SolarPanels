7464c915205517d38003d23d58c1290968a3c9c2
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-13 03:16:45 +0300

    added the client key and client secret values

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
-passport.use(new GoogleStrategy({
-    //option for the strategy
+passport.use(
+    new GoogleStrategy({
+        //option for the strategy
+        clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com",
+        clientSecret:"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"
 
-}),()=>{
-    // passport callback function
-})
+    }), () => {
+        // passport callback function
+    })
