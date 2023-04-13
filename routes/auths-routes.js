ae4ffd015c095b636c0e5f878c5115f2525f8362
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-13 20:24:29 +0300

    fixed a bug in auth-routes.js

--- routes/auths-routes.js
+++ routes/auths-routes.js
@@ -2,19 +2,19 @@ const passport = require('passport');
 
 const router = require('express').Router();
 
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
     res.render('login');
 });
 
 // auth logout
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
     // handle with passport
     res.send('logging out')
 })
 
 // auth with google
 
-router.get('/google',passport.authenticate('google',{ scope: ['profile']}),
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }),
 );
 
 module.exports = router;
