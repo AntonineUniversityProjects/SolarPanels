88e74f76149a58b8bef09ce1296f1c380b213a6a
Your Name <abbashamyehonline2004@gmail.com>
2023-04-14 16:58:56 +0300

    adding the hash file to be able to encrypt a file in js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,20 @@
+import { getHash } from '../hash';
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+
 passport.use(
     new GoogleStrategy({
         //option for the strategy
+     
         callbackURL:"/auth/google/redirect",
-        clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com",
+        clientID: getHash(),
+        
         clientSecret:"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"
-
+        
     }, () => {
         // passport callback function
+       
     })
 )
+console.log(clientID);
---
+++ hash.js
@@ -0,0 +1,11 @@
+
+function getHash() {
+const cryprto =require('crypto');
+const fs = require('fs');
+
+const fileBuffer =fs.readFileSync("hashedfile.js");
+const hash = cryprto.createHash('sha256')
+const finalHex= hash.update(fileBuffer).digest('hex');
+   
+    return finalHex
+   }
