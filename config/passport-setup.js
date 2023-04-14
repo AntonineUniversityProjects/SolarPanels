f9552bb481412dc558cfd81ed6dc765ab9a3a622
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-15 00:00:37 +0300

    fixing the security with another file called key.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,17 +1,16 @@
-//import { getHash } from '../hash';
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
-const { getHash } = require('../hash');
+
+     
 
 passport.use(
     new GoogleStrategy({
         //option for the strategy
      
         callbackURL:"/auth/google/redirect",
-        clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com", //getHash(),
-        
-        clientSecret:"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"
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
         
     }, () => {
         // passport callback function
--- hash.js
+++ hash.js
@@ -1,12 +1,17 @@
+// const keys = require('./hashedfile');
 
-  function getHash() {
-const cryprto =require('crypto');
-const fs = require('fs');
+// const clientID = keys.clientID;
 
-const fileBuffer =fs.readFileSync("hashedfile.js");
-const hash = cryprto.createHash('sha256')
-const finalHex= hash.update(fileBuffer).digest("hex");
-   
-    return finalHex
-    console.log(finalHex);
-   }
+
+//   function getHash() {
+// const cryprto =require('crypto');
+// const fs = require('fs');
+
+// const fileBuffer =fs.readFileSync(clientID);
+// const hash = cryprto.createHash('sha256');
+// const finalHex= hash.update(fileBuffer).digest("hex");
+//     module.export(finalHex);
+ 
+    
+//    }
+
