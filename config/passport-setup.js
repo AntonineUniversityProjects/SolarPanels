8aa361578cf317db80f033cbbc0ef225c2418e27
Your Name <abbashamyehonline2004@gmail.com>
2023-04-14 22:51:51 +0300

    some modifications

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,13 +2,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const { getHash } = require('../hash');
 
 passport.use(
     new GoogleStrategy({
         //option for the strategy
      
         callbackURL:"/auth/google/redirect",
-        clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com",// getHash(),
+        clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com", //getHash(),
         
         clientSecret:"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"
         
--- hash.js
+++ hash.js
@@ -1,11 +1,12 @@
 
-function getHash() {
+  function getHash() {
 const cryprto =require('crypto');
 const fs = require('fs');
 
 const fileBuffer =fs.readFileSync("hashedfile.js");
 const hash = cryprto.createHash('sha256')
-const finalHex= hash.update(fileBuffer).digest('hex');
+const finalHex= hash.update(fileBuffer).digest("hex");
    
     return finalHex
+    console.log(finalHex);
    }
