b51fc4fc72f5e86bae851552ea08aa74cabc274f
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-24 00:07:40 +0300

    s

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,12 @@ module.exports = {
     google: {
          clientID:"1024970547894-efl5r8lhsk5ql5uohp51f5qr5m55hbl8.apps.googleusercontent.com",
         clientSecret:"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"
+    },
+    mongodb:{
+         dbauth:"mongodb+srv://roygebrayel:313@solarpanels-login.oiy6gcx.mongodb.net/?retryWrites=true&w=majority",
+    
     }
+       
    
 };
     
