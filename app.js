f4ad90d8ff21b2b87baf301829936a9fdca3c2d3
Roygebrayel <roy.gebrayel11@gmail.com>
2023-04-24 00:17:50 +0300

    aa

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const passportSetup = require('./config/passport-setup');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// Handle form submission
-app.get('/submit-form', function (req, res) {
-    // Get form data from request body
-    const name = req.body.name;
-    const email = req.body.email;
-});
 
 //set up view engine
 app.set('view engine','ejs');
@@ -21,7 +14,7 @@ app.use('/auth',authRoutes);
 
 app.get('/',(req,res)=>{
 res.render('login');
-});
+})
 
 app.listen(3000,() => {
     console.log('app now listening for requests on port 3000');
