f0be9919c894dd4d233230f34e97dd78ba8beef7
Your Name <abbashamyehonline2004@gmail.com>
2023-05-03 00:07:04 +0300

    adding the navigation bar and the css for it

--- assets/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "vendor/autoload.php";
-$client = new MongoDB\Driver\Manager('');
-$db = $client->test;
-$collection = $db->users;
-$result = $collection->find();
-foreach ($result as $document) {
-    var_dump($document);
-}
