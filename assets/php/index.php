2eccc3eaa32327b4866757fe49eb82e4aaf8430c
Roygebrayel <roy.gebrayel11@gmail.com>
2023-05-03 01:57:13 +0300

    added the mongodb driver to the project using composer

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,9 +1,20 @@
 <?php
-require_once "vendor/autoload.php";
-$client = new MongoDB\Driver\Manager('');
-$db = $client->test;
-$collection = $db->users;
-$result = $collection->find();
-foreach ($result as $document) {
-    var_dump($document);
-}
+
+require 'vendor/autoload.php'; // include Composer's autoloader
+
+// connect to the MongoDB server
+$mongoClient = new MongoDB\Client("mongodb+srv://roygebrayel:313@solarpanels-login.oiy6gcx.mongodb.net/?retryWrites=true&w=majority");
+
+// select the database
+$mongoDB = $mongoClient->solarpanels;
+
+// select the collection
+$collection = $mongoDB->users;
+// insert a new document
+$document = array("name" => "John", "age" => 30);
+$collection->insertOne($document);
+
+var_dump($collection->getInsertedId);
+
+
+?>
