49f8b4c5145fc71aa27564f4c253d3590fc751df
Roygebrayel <roy.gebrayel11@gmail.com>
2023-05-02 22:58:22 +0300

    trying to add php

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,19 +1,9 @@
 <?php
-// connect to mongodb
-$client = new MongoDB\Client('mongodb+srv://roygebrayel:313@solarpanels-login.oiy6gcx.mongodb.net/?retryWrites=true&w=majority');
-//   $m = new MongoClient();
-  echo "Connection to database successfully";
-  // select a database
-  $db = $m->examplesdb;
-  echo "Database examplesdb selected";
-  $collection = $db->examplescol;
-  echo "Collection selected succsessfully";
-  $document = array( 
-  "title" => "MongoDB", 
-  "description" => "database", 
-  "likes" => 100,
-  "url" => "http://www.data-flair.training/mongodb/",
-  "by" => "data flair"
- );
-$collection->insert($document);
-echo "Document inserted successfully";
+require_once "vendor/autoload.php";
+$client = new MongoDB\Driver\Manager('');
+$db = $client->test;
+$collection = $db->users;
+$result = $collection->find();
+foreach ($result as $document) {
+    var_dump($document);
+}
