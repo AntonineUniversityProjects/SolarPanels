d875c8d25b522c7ba2ce82b1b20f5665ebc22aae
Roygebrayel <roy.gebrayel11@gmail.com>
2023-05-16 01:17:43 +0300

    trying to debug the login

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,20 +31,22 @@ return false;
 // If all checks pass, return true
 return true;
 }
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#password");
 
-togglePassword.addEventListener("click", function () {
-  // toggle the type attribute
-  const type = password.getAttribute("type") === "password" ? "text" : "password";
-  password.setAttribute("type", type);
 
-  // toggle the icon
-  this.classList.toggle("bi-eye");
-});
+// const togglePassword = document.querySelector("#togglePassword");
+// const password = document.querySelector("#password");
 
-// prevent form submit
-const form = document.querySelector("form");
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-});
+// togglePassword.addEventListener("click", function () {
+//   // toggle the type attribute
+//   const type = password.getAttribute("type") === "password" ? "text" : "password";
+//   password.setAttribute("type", type);
+
+//   // toggle the icon
+//   this.classList.toggle("bi-eye");
+// });
+
+// // prevent form submit
+// const form = document.querySelector("form");
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+// });
